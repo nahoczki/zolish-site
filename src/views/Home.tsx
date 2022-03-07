b3232aeac0ae08cb635c306e26a53ccab6257894
nahoczki <nnahoczki@gmail.com>
2022-03-06 19:16:53 -0600

    init

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,15 +1,31 @@
 import React from "react";
 import styled from "styled-components";
+import Particles from "react-tsparticles";
+import {particlesOptions} from "../helpers/particleOptions";
+import {IParticlesProps} from "react-tsparticles/ts3.4";
 
 const Home = () => {
 
     const Wrapper = styled.div`
+      display: flex;
+      width: 100%;
+      height: 100vh;
+      align-items: center;
+      justify-content: center;
       position: absolute;
+    `
+
+    const Text = styled.h1`
+      z-index: 10;
     `
 
     return (
         <Wrapper>
-            <h1>Coming Soon</h1>
+            <Text>Coming Soon</Text>
+            <Particles
+                id="tsparticles"
+                options={particlesOptions as IParticlesProps.options}
+            />
         </Wrapper>
     )
 }
